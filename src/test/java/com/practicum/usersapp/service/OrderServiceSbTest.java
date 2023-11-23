70ef30ea0cd1bc3756d6aa524dae677a4ec35219
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2023-11-24 01:42:24 +0700

    tests: добавил тест сервиса с помощью spring-boot

--- src/test/java/com/practicum/usersapp/service/OrderServiceSbTest.java
+++ src/test/java/com/practicum/usersapp/service/OrderServiceSbTest.java
@@ -3,23 +3,25 @@ package com.practicum.usersapp.service;
 import com.practicum.usersapp.dao.OrderRepository;
 import com.practicum.usersapp.dto.OrderDto;
 import com.practicum.usersapp.mappers.OrderMapper;
+import com.practicum.usersapp.mappers.OrderMapperImpl;
 import com.practicum.usersapp.model.OrderEntity;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ContextConfiguration;
 
-@ExtendWith(MockitoExtension.class)
-class OrderServiceTest {
+@SpringBootTest(classes = {OrderService.class, OrderMapperImpl.class})
+@ContextConfiguration(classes = {OrderService.class, OrderMapperImpl.class})
+class OrderServiceSbTest {
 
-  @Mock private OrderMapper userMapper;
-  @Mock private OrderRepository orderRepository;
-
-  private @InjectMocks OrderService orderService;
+  @MockBean private OrderRepository orderRepository;
+  @Autowired private OrderService orderService;
+  @SpyBean private OrderMapper orderMapper;
 
   @Test
   void findById_returnsOrderDtoWhenFound() {
@@ -32,16 +34,12 @@ class OrderServiceTest {
     OrderDto expectedOrderDto =
         OrderDto.builder().id(orderId).description("Утюг").status("NEW").build();
 
-    Mockito.when(userMapper.toDto(existingOrder)).thenReturn(expectedOrderDto);
-
     OrderDto actualOrderDto = orderService.findById(orderId);
 
     Assertions.assertEquals(expectedOrderDto, actualOrderDto);
-    
     Mockito.verify(orderRepository, Mockito.times(1)).findById(orderId);
     Mockito.verifyNoMoreInteractions(orderRepository);
 
-    Mockito.verify(userMapper, Mockito.times(1)).toDto(existingOrder);
-    Mockito.verifyNoMoreInteractions(userMapper);
+    Mockito.verify(orderMapper, Mockito.times(1)).toDto(existingOrder);
   }
 }
